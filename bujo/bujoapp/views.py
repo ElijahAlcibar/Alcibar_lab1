94de13e9c0a1ecacedd769f236bb78f18354af6b
ElijahAlcibar <elijah.alcibar@obf.ateneo.edu>
2021-04-05 06:42:11 +0800

    Home Page Functional Second Commit

--- bujo/bujoapp/views.py
+++ bujo/bujoapp/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
 
-def index(request):
-	return HttpResponse('Home Page')
+from .forms import NameOfUser
+
+def HomePageView(request):
+	if request.method == 'POST':
+		form = NameOfUser(request.POST)
+		if form.is_valid():
+			return HttpResponse(
+				"Hello, {}! Today is going to be a great day".format(
+					form.cleaned_data['name']
+				)
+			)
+	else:
+		form = NameOfUser()
+	return render(request, 'index.html', {'form': form})
