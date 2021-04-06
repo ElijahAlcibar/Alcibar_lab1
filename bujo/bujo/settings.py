64a5db188b7ce18adce2a57f046281091cf65f16
ElijahAlcibar <elijah.alcibar@obf.ateneo.edu>
2021-04-06 16:26:23 +0800

    requirements.txt

--- bujo/bujo/settings.py
+++ bujo/bujo/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't-82t%y7lkpn=uku=&7^(i*cf=13fz)vf$8e6&%+0lf(06vec^'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
